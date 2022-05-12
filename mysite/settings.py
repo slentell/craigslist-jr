1c3b7e2db84e0250eb17e4d051e99e6f37c5464a
slentell <slentell@aol.com>
2022-05-12 12:44:09 -0400

    updated files for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'craigslistjr',
     'djmoney',
     'crispy_forms',
+    
 ]
 
 MIDDLEWARE = [
